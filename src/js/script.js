5e6fb18a7f761c7a14809339712e224df776a206
Sarkissian-Telmane <telmane.sarkissian@edu.univ-fcomte.fr>
2025-11-04 17:18:21 +0100

    fin tp1

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,16 @@
 // TODO
+const aMettreEnRouge = document.querySelector("#a-mettre-en-rouge");
+aMettreEnRouge.style.color = 'red';
+
+const enrougesuiteaclick = document.querySelector("#en-rouge-suite-a-click");
+enrougesuiteaclick.addEventListener("click", (evt)=>{
+    enrougesuiteaclick.style.color = "red";
+});
+
+const h2 = document.querySelectorAll("h2")
+h2.forEach((elm)=>{
+    elm.addEventListener("click", (evt)=>{
+        evt.target.style.color = "red";
+    });
+   
+});
